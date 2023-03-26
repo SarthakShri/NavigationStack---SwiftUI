25f6f8b17dcd86a5ebaad36427f8c53f799b9aee
Sarthak Shrivastava <sarthakrishu@gmail.com>
2023-03-26 12:26:13 +0530

    Initial Commit

---
+++ NavStack/NavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackApp.swift
+//  NavStack
+//
+//  Created by Sarthak Shrivastava on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
